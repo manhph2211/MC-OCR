5d244b945ea58e6acec3569778feb84f400a313a
manhph2211 <maxph22112000@gmail.com>
2021-05-19 11:42:36 +0700

    update

--- maskrcnn/local_utils.py
+++ maskrcnn/local_utils.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from engine import train_one_epoch, evaluate
 import utils
 import transforms as T
+import numpy as np 
 
 
 def read_json(path):
@@ -24,14 +25,27 @@ def get_transform(train):
     return T.Compose(transforms)
 
 
+def remove_background(img,mask):
+	img = np.asarray(img)
+	mask = np.asarray(mask)
+	return (img.transpose(2,0,1) * mask * 255).transpose(1,2,0)
+
+
 if __name__ == '__main__':	
 	data = read_json(config.export_data_test_path)
 	img_path,mask_path = next(iter(data.items()))
 	img = Image.open(img_path)
 	mask = Image.open(mask_path)
+	print(np.asarray(mask).shape)
+	print(np.asarray(img).shape)
+	removed_bg_img = remove_background(img,mask)
 	mask.putpalette([0,0,0,255,0,0])
-	plt.subplot(1,2,1)
+	plt.subplot(1,3,1)
 	plt.imshow(img)
-	plt.subplot(1,2,2)
+	plt.subplot(1,3,2)
 	plt.imshow(mask)
+	plt.subplot(1,3,3)
+	plt.imshow(removed_bg_img)
 	plt.show()
+
+
