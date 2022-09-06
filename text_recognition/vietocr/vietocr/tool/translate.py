2eee00e79a11a591b3e4b746db2ffde8447ce0dc
Phạm Hùng Mạnh (VNCDLL-CTÐTKSAI-HVCTÐTKSAI) <manhph5@vingroup.net>
2022-09-06 13:34:31 +0700

    demo reg

--- text_recognition/vietocr/vietocr/tool/translate.py
+++ text_recognition/vietocr/vietocr/tool/translate.py
@@ -4,9 +4,9 @@ import math
 from PIL import Image
 from torch.nn.functional import log_softmax, softmax
 
-from vietocr.model.transformerocr import VietOCR
-from vietocr.model.vocab import Vocab
-from vietocr.model.beam import Beam
+from vietocr.vietocr.model.transformerocr import VietOCR
+from vietocr.vietocr.model.vocab import Vocab
+from vietocr.vietocr.model.beam import Beam
 
 def batch_translate_beam_search(img, model, beam_size=4, candidates=1, max_seq_length=128, sos_token=1, eos_token=2):
     # img: NxCxHxW
