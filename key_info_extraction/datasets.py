0daa14ff375a745d652e7227ce62e9449c82f37d
Đoàn Ngọc Phú (VNCDLL-CTÐTKSAI-HVCTÐTKSAI) <phudn4@vingroup.net>
2022-09-08 00:06:07 +0700

    Move phobert into dataset

--- key_info_extraction/datasets.py
+++ key_info_extraction/datasets.py
@@ -11,8 +11,8 @@ import matplotlib.pyplot as plt
 import torch
 from torch_geometric.data import Dataset
 from torch_geometric.utils.convert import from_networkx
-from utils import ID2LABEL, LABEL2ID
-from transformers import AutoTokenizer
+from .utils import ID2LABEL, LABEL2ID
+from transformers import AutoTokenizer, AutoModel
 
 
 class Receipt(Dataset):
@@ -22,15 +22,20 @@ class Receipt(Dataset):
         with open(json_file, 'r') as f:
             self.data = json.load(f)
 
+        print(self.data)
         self.filenames = list(self.data.keys())
+        self.embedding = AutoModel.from_pretrained(bert_model)
 
     def connect(self, bboxes, imgw, imgh):
         G = nx.Graph()
         for src_idx, src_row in enumerate(bboxes):
-            if not src_row.get('label', False):
+            if not src_row.get('label', None):
                 src_row['label'] = "OTHER"
             src_row['y'] = torch.tensor([LABEL2ID[src_row['label']]], dtype=torch.long)
-            src_row["x_max"], src_row["y_max"], src_row["x_min"], src_row["y_min"] = src_row["crop"][0][0], src_row["crop"][0][1], src_row["crop"][1][0], src_row["crop"][1][1]
+            src_row["x_max"], src_row["y_max"], src_row["x_min"], src_row["y_min"] = src_row["crop"][0][0], \
+                                                                                     src_row["crop"][0][1], \
+                                                                                     src_row["crop"][1][0], \
+                                                                                     src_row["crop"][1][1]
             src_row['bbox'] = list(map(float, [
                 src_row["x_min"] / imgw,
                 src_row["y_min"] / imgh,
@@ -65,20 +70,23 @@ class Receipt(Dataset):
             for dest_idx, dest_row in enumerate(bboxes):
                 if dest_idx == src_idx:
                     continue
-                dest_row["x_min"], dest_row["y_min"], dest_row["x_max"], dest_row["y_max"] = dest_row["crop"][1][0], dest_row["crop"][1][1], dest_row["crop"][0][0], dest_row["crop"][0][1]
+                dest_row["x_min"], dest_row["y_min"], dest_row["x_max"], dest_row["y_max"] = dest_row["crop"][1][0], \
+                                                                                             dest_row["crop"][1][1], \
+                                                                                             dest_row["crop"][0][0], \
+                                                                                             dest_row["crop"][0][1]
                 dest_range_x = (dest_row["x_min"], dest_row["x_max"])
                 dest_range_y = (dest_row["y_min"], dest_row["y_max"])
                 dest_center_x, dest_center_y = np.mean(dest_range_x), np.mean(dest_range_y)
 
                 # Find box in vertical must have common x range.
-                if max(src_range_x[0], dest_range_x[0]) < min(src_range_x[1], dest_range_x[1]):
+                if max(src_range_x[0], dest_range_x[0]) <= min(src_range_x[1], dest_range_x[1]):
                     # Find underneath box: neighbor yminx must be smaller than source ymax
                     if dest_range_y[0] >= src_range_y[1]:
                         neighbor_vert_bot.append(dest_idx)
                     elif dest_range_y[1] <= src_range_y[0]:
                         neighbor_vert_top.append(dest_idx)
                 # Find box in horizontal must have common y range.
-                if max(src_range_y[0], dest_range_y[0]) < min(src_range_y[1], dest_range_y[1]):
+                if max(src_range_y[0], dest_range_y[0]) <= min(src_range_y[1], dest_range_y[1]):
                     # Find right box: neighbor xmin must be smaller than source xmax
                     if dest_range_x[0] >= src_range_x[1]:
                         neighbor_hozi_right.append(dest_idx)
@@ -88,12 +96,12 @@ class Receipt(Dataset):
             neighbors = []
 
             if neighbor_hozi_left:
-                nei = max(neighbor_hozi_left, key=lambda x: bboxes[x]['x_max'] - src_center_x)
+                nei = min(neighbor_hozi_left, key=lambda x: src_center_x - bboxes[x]['x_max'])
                 neighbors.append(nei)
                 G.add_edge(src_idx, nei)
             if neighbor_hozi_right:
                 nei = min(neighbor_hozi_right,
-                          key=lambda x: src_center_x - bboxes[x]['x_min'])
+                          key=lambda x: bboxes[x]['x_min'] - src_center_x)
                 neighbors.append(nei)
                 G.add_edge(src_idx, nei)
             if neighbor_vert_bot:
@@ -101,7 +109,7 @@ class Receipt(Dataset):
                 neighbors.append(nei)
                 G.add_edge(src_idx, nei)
             if neighbor_vert_top:
-                nei = min(neighbor_vert_top, key=lambda x: bboxes[x]['y_max'])
+                nei = min(neighbor_vert_top, key=lambda x: src_center_y - bboxes[x]['y_max'])
                 neighbors.append(nei)
                 G.add_edge(src_idx, nei)
 
@@ -115,18 +123,18 @@ class Receipt(Dataset):
             idx = idx.tolist()
 
         filename = self.filenames[idx]
-        filepath = '../data/mcocr_public_train_test_shared_data/mcocr_train_data/train_images/' + filename + '.jpg' 
+        filepath = '../data/mcocr_public_train_test_shared_data/mcocr_train_data/train_images/' + filename + '.jpg'
         bboxes = self.data[filename]
 
         G = self.connect(bboxes=bboxes,
                          imgw=1000, imgh=1000)
-        nx.draw(G,node_size= 20)
-        if not os.path.isfile('./vis/image_'+str(idx)+'.png'):
-            plt.savefig('./vis/image_'+str(idx)+'.png')
+        # nx.draw(G,node_size= 20)
+        # if not os.path.isfile('./vis/image_'+str(idx)+'.png'):
+        #     plt.savefig('./vis/image_'+str(idx)+'.png')
         data = from_networkx(G)
 
         token = self.tokenizer(data.text, add_special_tokens=True, truncation=True,
-                              max_length=128, padding='max_length', return_tensors='pt')
+                               max_length=128, padding='max_length', return_tensors='pt')
         data.input_ids, data.attention_mask = token.input_ids, token.attention_mask
         data.text_len = torch.count_nonzero(data.input_ids, dim=1) / 128.0
         data.text_len = torch.unsqueeze(data.text_len, dim=1)
@@ -136,6 +144,9 @@ class Receipt(Dataset):
 
         data.path = filepath
         data.imname = self.filenames[idx]
+        bert_output = self.embedding(attention_mask=data.attention_mask,
+                                     input_ids=data.input_ids)
+        data.embedding = bert_output['last_hidden_state'][:, 0]
 
         return data
 
--- key_info_extraction/models/phobert_sage.py
+++ key_info_extraction/models/phobert_sage.py
@@ -3,13 +3,11 @@ import torch.nn.functional as F
 from torch_geometric.nn import SAGEConv
 from key_info_extraction.utils import ID2LABEL
 
-from transformers import AutoModel, AutoTokenizer
-
 
 class SageNet(nn.Module):
-    def __init__(self, in_channels, n_classes=len(ID2LABEL.keys()), dropout_rate=0.2, bert_model='vinai/phobert-base'):
+    def __init__(self, in_channels, n_classes=len(ID2LABEL.keys()), dropout_rate=0.2, bert_model='vinai/phobert-base',
+                 device='cuda'):
         super(SageNet, self).__init__()
-        self.embedding = AutoModel.from_pretrained(bert_model)
         self.conv1 = SAGEConv(in_channels=in_channels, out_channels=64)
         self.relu1 = nn.ReLU()
         self.conv2 = SAGEConv(in_channels=64, out_channels=32)
@@ -23,11 +21,8 @@ class SageNet(nn.Module):
     def forward(self, data):
 
         edge_index, edge_weight = data.edge_index, data.edge_attr
-        bert_output = self.embedding(attention_mask=data.attention_mask,
-                                     input_ids=data.input_ids)
-        x = bert_output['last_hidden_state'][:, 0]
 
-        x = F.dropout(self.relu1(self.conv1(x, edge_index, edge_weight)), p=self.dropout_rate)
+        x = F.dropout(self.relu1(self.conv1(data.embedding, edge_index, edge_weight)), p=self.dropout_rate)
         x = F.dropout(self.relu2(self.conv2(x, edge_index, edge_weight)), p=self.dropout_rate)
         x = F.dropout(self.relu3(self.conv3(x, edge_index, edge_weight)), p=self.dropout_rate)
         x = self.conv4(x, edge_index, edge_weight)
